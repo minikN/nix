7495f51826d230163411e2e792d66af929ec0c04
Demis Balbach <db@minikn.xyz>
2023-06-30 23:17:38 +0200

    config: Add gpg module

--- modules/common/gpg.nix
+++ modules/common/gpg.nix
@@ -7,15 +7,23 @@
 {
   home-manager.users.${config.user} = {
     services = {
+     
+     ## Enable gpg-agent with ssh support
      gpg-agent.enable = true;
      gpg-agent.enableSshSupport = true;
+
+     ## Add SSH key
+     gpg-agent.sshKeys = [ "E3FFA5A1B444A4F099E594758008C1D8845EC7C0" ];
     };
 
     programs = {
       gpg = {
+        
+        ## Enable GnuPG
         enable = true;
-        #homeDir = "${config.xdg.dataHome}/gnupg";
-        #homeDir = "${con/gnupg";
+
+        ## Set homedir to `$XDG_DATA_HOME/gnupg'. This will set GNUPG_HOME.
+        homedir = "${config.home-manager.users.${config.user}.xdg.dataHome}/gnupg";
       };
     };
   };
