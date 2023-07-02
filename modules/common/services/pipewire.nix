9030360dae4d168171d8916580d2463bda501b44
Demis Balbach <db@minikn.xyz>
2023-07-02 23:29:07 +0200

    pipewire: Enabling media keys

--- modules/common/services/pipewire.nix
+++ modules/common/services/pipewire.nix
@@ -39,17 +39,33 @@
     
       ## Enabling PulseAudio system tray
       services.pasystray.enable = true;
+
+      home.packages = [ pkgs.pulseaudioFull ];
       
       ## Configure pavu to be floating
-      wayland.windowManager.sway.config.window = {
-        commands = lib.mkIf (config.os.wm == "sway") [
-          {
-            command = "floating enable, border pixel 2";
-            criteria = {
-              app_id = "pavucontrol";
-            };
-          }
-        ];
+      wayland.windowManager.sway = {
+        config.window = {
+          commands = lib.mkIf (config.os.wm == "sway") [
+            {
+              command = "floating enable, border pixel 2";
+              criteria = {
+                app_id = "pavucontrol";
+              };
+            }
+          ];
+        };
+
+        ## Making media keys work
+        extraConfig = ''
+          bindsym --locked XF86AudioRaiseVolume \
+          exec ${pkgs.pulseaudioFull}/bin/pactl set-sink-mute @DEFAULT_SINK@ false; \
+          exec ${pkgs.pulseaudioFull}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%
+          bindsym --locked XF86AudioLowerVolume \
+          exec pactl${pkgs.pulseaudioFull}/bin/pactl set-sink-mute @DEFAULT_SINK@ false; \
+          exec ${pkgs.pulseaudioFull}/bin/pactlpactl set-sink-volume @DEFAULT_SINK@ -5%
+          bindsym --locked XF86AudioMute exec ${pkgs.pulseaudioFull}/bin/pactlpactl set-sink-mute @DEFAULT_SINK@ toggle
+          bindsym --locked XF86AudioMicMute exec ${pkgs.pulseaudioFull}/bin/pactlpactl set-source-mute @DEFAULT_SOURCE@ toggle
+        '';
       };
     };
   };
