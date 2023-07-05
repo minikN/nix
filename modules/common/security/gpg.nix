5f7812f87c711d7c4e23481fced7fa3d04da15b2
Demis Balbach <db@minikn.xyz>
2023-07-05 22:28:09 +0200

    gpg: Change pinentry to qt

--- modules/common/security/gpg.nix
+++ modules/common/security/gpg.nix
@@ -30,9 +30,12 @@
     services = {
      
      ## Enable gpg-agent with ssh support
-     gpg-agent.enable = true;
-     gpg-agent.enableSshSupport = true;
-     gpg-agent.enableZshIntegration = config.os.shell == "zsh";
+     gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      enableZshIntegration = config.os.shell == "zsh";
+      pinentryFlavor = "qt";
+     };
 
      ## Add SSH key
      gpg-agent.sshKeys = [ "E3FFA5A1B444A4F099E594758008C1D8845EC7C0" ];
