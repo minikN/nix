fbde205e5bd8cdbf49e663e2fba30b3b95bc9f02
Demis Balbach <db@minikn.xyz>
2023-07-06 20:37:50 +0200

    xdg: Enable mimeApps; userDirs

--- modules/common/services/xdg.nix
+++ modules/common/services/xdg.nix
@@ -35,9 +35,32 @@
       extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     };
 
+    ## Needed so that the user dirs get exported as env vars
+    environment.systemPackages = [ pkgs.xdg-user-dirs ];
+
     ## Enabling XDG
     home-manager.users.${config.user} = {
       xdg.enable = true;
+      
+      ## Enable XDG mime type handling
+      xdg.mime.enable = true;
+      xdg.mimeApps.enable = true;
+      
+      ## Enable XDG user directories
+      xdg.userDirs = {
+        enable = true;
+        createDirectories = true;
+        documents = "${config.home-manager.users.${config.user}.home.homeDirectory}/Documents";
+        download = "${config.home-manager.users.${config.user}.home.homeDirectory}/Downloads";
+        pictures = "${config.home-manager.users.${config.user}.home.homeDirectory}/Pictures";
+        
+        ## Unused directories
+        desktop = null;
+        music = null;
+        publicShare = null;
+        templates = null;
+        videos = null;
+      };
     };
   };
 }
