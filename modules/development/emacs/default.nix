1dbeae9825874f45d67a3234ad7d1833fe3810bb
Demis Balbach <db@minikn.xyz>
2023-07-08 22:04:07 +0200

    development: Add emacs

--- modules/development/emacs/default.nix
+++ modules/development/emacs/default.nix
@@ -24,20 +24,73 @@
 ###
 ### CODE:
 
-{ self, config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
-{
+
+  let
+    #nurNoPkgs = import config.inputs.nur { pkgs = null; nurpkgs = pkgs; };
+  in {
+  imports = [
+    ./appearance.nix
+  ];
+  
   config = {
     home-manager.users.${config.user} = {
+      #imports = [
+      #  nur-no-pkgs.repos.rycee.hmModules.emacs-init
+      #];
       services.emacs = {
         enable = true;
         package = pkgs.emacs29-pgtk;
       };
+      
+
       programs.emacs = {
         enable = true;
         package = pkgs.emacs29-pgtk;
+        extraPackages = epkgs: [ epkgs.magit ];
+        extraConfig = ''
+          ;; Packages will be initialized by guix later.
+          (setq package-enable-at-startup nil)
+          (setq package-archives nil)
+
+          ;; Defer garbage collection further back in the startup process
+          (setq gc-cons-threshold most-positive-fixnum
+                gc-cons-percentage 0.6)
+
+          (add-hook 'emacs-startup-hook
+            (lambda ()
+              (setq undo-limit (* 8 1024 1024)
+	                  read-process-output-max (* 1024 1024))))
+
+          ;; Ignore X resources
+          (advice-add #'x-apply-session-resources :override #'ignore)
+          ;; TODO: Probably the better approach is:
+          ;; (setq inhibit-x-resources t)
+
+          ;; Do not resize the frame at this early stage.
+          (setq frame-inhibit-implied-resize t)
+          
+          (pixel-scroll-precision-mode 1)
+
+          ;; Set some defaults for the startup behaviour.
+          (setq inhibit-splash-screen t
+                inhibit-startup-screen t
+                inhibit-startup-buffer-menu t
+                inhibit-startup-echo-area-message "Welcome."
+                use-dialog-box t
+                use-file-dialog nil)
+
+          ;; Theme
+          (load-theme 'modus-operandi t)
+
+          ;; Font
+          (add-to-list 'default-frame-alist '(font . "${config.os.fonts.mono.regular}-${builtins.toString config.os.fonts.mono.size}"))
+          (set-face-attribute 'default t :font "${config.os.fonts.mono.regular}-${builtins.toString config.os.fonts.mono.size}")
+        '';
       };
     };
   };
-}
+  }
+
 
