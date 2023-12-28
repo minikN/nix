6fd3069d5a0b70fd078de107b44f84e66f7165c9
Demis Balbach <db@minikn.xyz>
2023-12-28 20:30:07 +0100

    services: Add ACPI.

--- modules/common/services/default.nix
+++ modules/common/services/default.nix
@@ -28,6 +28,10 @@
 {
   config = {
 
+    ## ACPI
+    services.acpid.enable = true;
+    services.acpid.logEvents = true;
+
     ## DBus
     services.dbus.enable = true;
     services.upower.enable = true;
