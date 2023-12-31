031f2da22abdb8c73f6132dbe3a2837882716a57
Demis Balbach <db@minikn.xyz>
2023-12-31 12:18:48 +0100

    printer: Fix service name.

--- modules/common/services/printer.nix
+++ modules/common/services/printer.nix
@@ -28,7 +28,7 @@
 {
   services.printing.enable = true;
   services.avahi.enable = true;
-  services.avahi.nssmdns = true;
+  services.avahi.nssmdns4 = true;
   # for a WiFi printer
   services.avahi.openFirewall = true;
 }
