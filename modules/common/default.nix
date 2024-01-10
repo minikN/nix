6853f6ff184c93dd8c33796636a3f9789fd02ab8
Demis Balbach <db@minikn.xyz>
2024-01-10 21:53:48 +0100

    default: Add archive manager.

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -236,6 +236,7 @@
               mpv
               realvnc-vnc-viewer
               gnome.gnome-system-monitor
+              gnome.file-roller
             ];
         }
         (lib.mkIf config.os.wayland {
