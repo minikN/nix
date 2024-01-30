354ff79f0c123ba0acab3a6a024711016e8de7ec
Demis Balbach <db@minikn.xyz>
2024-01-30 10:26:42 +0100

    filesystem: Change nas mount paths.

--- modules/common/system/filesystems/synology.nix
+++ modules/common/system/filesystems/synology.nix
@@ -31,7 +31,7 @@
 {
   environment.systemPackages = [ pkgs.cifs-utils ];
   fileSystems."/mnt/nas/movies" = {
-    device = "//192.168.178.26/Filme";
+    device = "//192.168.178.26/movies";
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
@@ -39,7 +39,7 @@
   };
 
   fileSystems."/mnt/nas/shows" = {
-    device = "//192.168.178.26/Serien";
+    device = "//192.168.178.26/shows";
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
@@ -47,30 +47,22 @@
   };
 
   fileSystems."/mnt/nas/music" = {
-    device = "//192.168.178.26/Musik";
+    device = "//192.168.178.26/music";
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
     in ["${automount_opts},credentials=/etc/nixos/smb-credentials,uid=1000,gid=100"];
   };
 
-    fileSystems."/mnt/nas/audiobooks" = {
-    device = "//192.168.178.26/Hoerbuecher";
+  fileSystems."/mnt/nas/audiobooks" = {
+    device = "//192.168.178.26/audiobooks";
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
     in ["${automount_opts},credentials=/etc/nixos/smb-credentials,uid=1000,gid=100"];
   };
 
-    fileSystems."/mnt/nas/roms" = {
-    device = "//192.168.178.26/ROMs";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-credentials,uid=1000,gid=100"];
-  };
-
-    fileSystems."/mnt/nas/temp" = {
+  fileSystems."/mnt/nas/temp" = {
     device = "//192.168.178.26/Temp";
     fsType = "cifs";
     options = let
