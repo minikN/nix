1d6b97cedf14a095b9b4543a46b3bcc26afc0314
Demis Balbach <db@minikn.xyz>
2024-01-31 08:25:57 +0100

    emacs: Add doc block.

--- modules/development/emacs/appearance/default.nix
+++ modules/development/emacs/appearance/default.nix
@@ -40,21 +40,25 @@
       programs.emacs.init = {
         enable = true;
         earlyInit = ''
-          (push '(menu-bar-lines . 0) default-frame-alist)
-          (push '(tool-bar-lines . 0) default-frame-alist)
-          (push '(vertical-scroll-bars) default-frame-alist)
-          (push '(horizontal-scroll-bars) default-frame-alist)
-          (push (cons 'left-fringe 8) default-frame-alist)
-          (push (cons 'right-fringe 8) default-frame-alist)
-          (push '(no-special-glyphs) default-frame-alist)
-          (push '(undecorated) default-frame-alist)
-          (setq menu-bar-mode nil
-                tool-bar-mode nil
-                scroll-bar-mode nil)
-          (push '(internal-border-width . 8) default-frame-alist)
-          (setq inhibit-startup-screen t)
-                (setq inhibit-startup-message t)
-                (setq initial-scratch-message nil)
+;; ~!emacs-lisp!~
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(horizontal-scroll-bars) default-frame-alist)
+(push (cons 'left-fringe 8) default-frame-alist)
+(push (cons 'right-fringe 8) default-frame-alist)
+(push '(no-special-glyphs) default-frame-alist)
+(push '(undecorated) default-frame-alist)
+(setq menu-bar-mode nil
+      tool-bar-mode nil
+      scroll-bar-mode nil)
+(push '(internal-border-width . 8) default-frame-alist)
+(setq inhibit-startup-screen t
+      inhibit-startup-message t
+      initial-scratch-message nil)
+
+;; suppress byte-compile warnings
+(setq byte-compile-warnings nil)
         '';
       };
 
