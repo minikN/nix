c756e81f2b8c6ce769e394cc128928d10f1cdf57
Demis Balbach <db@minikn.xyz>
2025-11-01 12:57:25 +0100

    flake: Uses correct HM branch.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
